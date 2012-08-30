c9aa7910092b17728c68b3a93af6fa8329474063
Hannes Mehnert <hannes@mehnert.org>
2012-08-30 09:41:46 +0200

    Revert "Define an extension point for a DamagerRepairerFactory,"
    Reason was that JavaEditor/CompilationUnitEditor do not use this stuff
    
    This reverts commit fc504ad5ac0cb42a5d676031f72823530be16f5c.

--- src/dk/itu/sdg/kopitiamaspects/DamagerRepairerFactoryInterface.java
+++
@@ -1,8 +0,0 @@
-package dk.itu.sdg.kopitiamaspects;
-
-import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
-import org.eclipse.jface.text.rules.ITokenScanner;
-
-public interface DamagerRepairerFactoryInterface {
-	public DefaultDamagerRepairer newDamagerRepairer (ITokenScanner scanner);
-}
